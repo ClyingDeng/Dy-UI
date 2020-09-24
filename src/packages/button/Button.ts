2523a4bd27107679b532783ae085a1313fd4923e
ClyingDeng <1174915260@qq.com>
2020-09-24 17:22:46 +0800

    按钮props

--- src/packages/button/Button.ts
+++ src/packages/button/Button.ts
@@ -1,7 +1,28 @@
-
-import { Component, Vue } from 'vue-property-decorator';
-
+import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class DyButton extends Vue {
+  @Prop({
+    default: '',
+    validator(type) {
+      if (
+        type &&
+        !['warning', 'success', 'danger', 'info', 'primary'].includes(type)
+      ) {
+        console.error(
+          'type类型必须为：' +
+            ['warning', 'success', 'danger', 'info', 'primary'].join('、'),
+        );
+      }
+      return true;
+    },
+  })
+  readonly type!: string;
 
+  get btnClass() {
+    let classes = [];
+    if (this.type) {
+      classes.push(`dy-btn-${this.type}`);
+    }
+    return classes;
+  }
 }
