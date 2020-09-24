daca76bf084583531b2ced79e48cab1cf0eb04e7
ClyingDeng <1174915260@qq.com>
2020-09-24 22:36:02 +0800

    icon

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
-<!--
- * @author: Clying
--->
 <template>
   <div id="app">
     <dy-button type="info">默认按钮</dy-button>
+    <dy-button>默认按钮</dy-button>
     <dy-button type="success">默认按钮</dy-button>
     <dy-button type="primary">默认按钮</dy-button>
     <dy-button type="warning">默认按钮</dy-button>
-    <dy-icon></dy-icon>
+    <dy-icon icon="play"></dy-icon>
+    <!-- 带icon的按钮 -->
+    <dy-button type="warning" icon="play">默认按钮</dy-button>
+    <dy-button type="warning" icon="play" icon-position="right"
+      >后面图标按钮</dy-button
+    >
   </div>
 </template>
 
--- src/packages/button/Button.ts
+++ src/packages/button/Button.ts
@@ -16,12 +16,26 @@ export default class DyButton extends Vue {
       return true;
     },
   })
-  readonly type!: string;
-
+  private readonly type!: string;
+  @Prop(String)
+  private readonly icon!: string;
+  @Prop({
+    default: 'left',
+    validator(type) {
+      if (!['left', 'right'].includes(type)) {
+        console.log('iconPosition属性必须为：' + 'left | right');
+      }
+      return true;
+    },
+  })
+  private readonly iconPosition!: string;
   get btnClass() {
     let classes = [];
     if (this.type) {
       classes.push(`dy-btn-${this.type}`);
+    }
+    if (this.iconPosition) {
+      classes.push(`dy-btn-${this.iconPosition}`);
     }
     return classes;
   }
