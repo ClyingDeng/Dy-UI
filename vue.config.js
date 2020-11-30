58822112f832a89ec578213418d38c58d710a327
ClyingDeng <1174915260@qq.com>
2020-11-30 21:29:15 +0800

    xiu

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,17 @@
-const webpack = require('webpack');
 module.exports = {
   transpileDependencies: ['vue-class-component'],
   configureWebpack: {
+    performance: {
+      hints: 'warning',
+      // 入口起点的最大体积
+      maxEntrypointSize: 50000000,
+      // 生成文件的最大体积
+      maxAssetSize: 30000000,
+      // 只给出 js 文件的性能提示
+      assetFilter: function (assetFilename) {
+        return assetFilename.endsWith('.js')
+      }
+    }
   },
   chainWebpack: config => {
     config
@@ -11,7 +21,7 @@ module.exports = {
     const scssRes = config.module.rule('scss').oneOfs.store;
     scssRes.forEach(item => {
       item
-      .use("sass-resource-loader")
+        .use("sass-resource-loader")
         .loader('sass-resources-loader')
         .options({
           resources: './src/styles/_var.scss'
@@ -23,7 +33,7 @@ module.exports = {
     // hot:true,
     host: 'localhost',
     port: 8888,
-    open : true,
+    open: true,
     // https: false
   }
 }
