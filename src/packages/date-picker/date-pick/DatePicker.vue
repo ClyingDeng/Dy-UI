4c714e4a14805d7cba13ce63cfa289286f73f6ae
ClyingDeng <1174915260@qq.com>
2020-12-28 20:31:22 +0800

    日历头

--- src/packages/date-picker/date-pick/DatePicker.vue
+++ src/packages/date-picker/date-pick/DatePicker.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="dy-date-picker">
-    <dy-input suffix-icon="dy-icon-rili"></dy-input>
-    <div class="dy-date-content">
+  <div class="dy-date-picker" v-clickOutSide="handleBlur">
+    <dy-input
+      suffix-icon="dy-icon-rili"
+      @focus="handleFocus"
+      :value="formateDate"
+    ></dy-input>
+    <div class="dy-date-content" v-if="isVisible">
       <div class="dy-date-picker-header">
         <dy-icon icon="dy-icon-doubleleft"></dy-icon>
         <dy-icon icon="dy-icon-arrow-left"></dy-icon>
-        <span>xxxx年 xx月 xx日</span>
+        <span>{{ tempTime.year }}年 {{ tempTime.month + 1 }}月</span>
         <dy-icon icon="dy-icon-arrow-right"></dy-icon>
         <dy-icon icon="dy-icon-doubleright"></dy-icon>
       </div>
       <div class="dy-date-picker-content">
-        <template>dates</template>
-        <template>years</template>
-        <template>months</template>
+        <template v-if="mode === 'dates'">
+          <div>
+            <span class="cell" v-for="week in weeks" :key="week">{{week}}</span>
+          </div>
+        </template>
+        <template v-if="mode === 'years'">years</template>
+        <template v-if="mode === 'months'">months</template>
       </div>
     </div>
   </div>
