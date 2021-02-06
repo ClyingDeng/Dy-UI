a326689c9a718edc0d8bb62244e18b813f43b545
ClyingDeng <1174915260@qq.com>
2021-02-06 16:55:25 +0800

    轮播图按钮+测试

--- src/packages/carousel/carousel/Carousel.ts
+++ src/packages/carousel/carousel/Carousel.ts
@@ -30,34 +30,69 @@ export default class DyButton extends Vue {
       this.timer = setInterval(() => {
         let index = this.currentSelected;
         let newIndex = index + 1;
-        // 临界问题
-        if (newIndex === this.len) {
-          newIndex = 0;
-        }
-        if (newIndex === -1) {
-          newIndex = this.len - 1;
-        }
-        // 0 => 1 正 1=>0 反
-        this.reverse = index > newIndex ? true : false;
-        // 无缝滚动  0 => 2 反   2=> 0 正
-        if (this.loop) {
-          if (index === 0 && newIndex === this.len - 1) {
-            this.reverse = true;
-          }
-          if (index === this.len - 1 && newIndex === 0) {
-            this.reverse = false;
-          }
-        }
-        // 控制子组件
-        this.$children.forEach((vm: any) => {
-          // 用于更改子组件的数据
-          vm.reverse = this.reverse;
-        });
-        this.$nextTick(() => {
-          this.currentSelected = newIndex;
-        });
+        this.select(newIndex);
       }, this.delay);
     }
+  }
+  private select(newIndex: number, flag?: boolean) {
+    let index = this.currentSelected;
+    // 临界问题
+    if (newIndex === this.len) {
+      newIndex = 0;
+    }
+    if (newIndex === -1) {
+      newIndex = this.len - 1;
+    }
+    // 0 => 1 正 1=>0 反
+    this.reverse = index > newIndex ? true : false;
+    // 无缝滚动  0 => 2 反   2=> 0 正
+    if ((this.timer || flag) && this.loop) {
+      if (index === 0 && newIndex === this.len - 1) {
+        this.reverse = true;
+      }
+      if (index === this.len - 1 && newIndex === 0) {
+        this.reverse = false;
+      }
+    }
+    // 控制子组件
+    this.$children.forEach((vm: any) => {
+      // 用于更改子组件的数据
+      vm.reverse = this.reverse;
+    });
+    this.$nextTick(() => {
+      this.currentSelected = newIndex;
+    });
+  }
+  private handleMouseEnter() {
+    clearInterval(this.timer);
+    this.timer = null;
+  }
+  private handleMouseLeave() {
+    this.run();
+  }
+  private handleTouchStart(e: any) {
+    this.handleMouseEnter();
+    // 需要记录拖拽前的位置
+    this.startTouch = e.touches[0];
+  }
+  private handleTouchEnd(e: any) {
+    let endTouch = e.changedTouches[0];
+
+    let { clientX: x1, clientY: y1 } = this.startTouch;
+    let { clientX: x2, clientY: y2 } = endTouch;
+    let distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    // 是往哪边移动
+
+    let disY = Math.abs(y2 - y1);
+    let x = (Math.sqrt(2) / 2) * distance;
+    if (disY < x) {
+      if (x2 > x1) {
+        this.select(this.currentSelected - 1, true);
+      } else {
+        this.select(this.currentSelected + 1, true);
+      }
+    }
+    this.run();
   }
   private beforeDestroy() {
     clearInterval(this.timer);
--- src/packages/carousel/carousel/Carousel.vue
+++ src/packages/carousel/carousel/Carousel.vue
@@ -1,7 +1,34 @@
 <template>
-  <div class="dy-carousel" :style="{ height }">
+  <div
+    class="dy-carousel"
+    :style="{ height }"
+    @mouseenter="handleMouseEnter"
+    @mouseleave="handleMouseLeave"
+    @touchstart="handleTouchStart"
+    @touchend="handleTouchEnd"
+  >
     <div class="viewport">
       <slot></slot>
+    </div>
+    <div class="dots">
+      <span
+        v-for="item in len"
+        :key="item"
+        :class="{ active: item - 1 === currentSelected }"
+        @click="select(item - 1)"
+      >
+        {{ item - 1 }}
+      </span>
+    </div>
+    <div class="arrow-box">
+      <dy-button
+        icon="dy-icon-arrow-left"
+        @click="select(currentSelected - 1, true)"
+      ></dy-button>
+      <dy-button
+        icon="dy-icon-arrow-right"
+        @click="select(currentSelected + 1, true)"
+      ></dy-button>
     </div>
   </div>
 </template>
